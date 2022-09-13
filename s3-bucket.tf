945073999d88845cbaede16fcf232ac58486560b
blackhuny <blackhuny@daum.net>
2022-09-13 17:45:14 +0900

    Delete s3-bucket.tf

--- s3-bucket.tf
+++
@@ -1,4 +0,0 @@
-variable "bucket_prefix" {
-  description = "Description: (Optional, Forces new resource) Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
-  default     = null  
-}
